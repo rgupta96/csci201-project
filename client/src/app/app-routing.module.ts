fd392f0c24c8aef244049ee7014449c84c3741a1
rgupta96 <rgupta96@usc.edu>
2021-12-06 03:07:32 -0800

    get rid of unneeded files

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,16 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: 'listings',
     loadChildren: () => import('./listings/listings.module').then( m => m.ListingsPageModule)
-  },
-  {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
   },
   {
     path: 'login',
