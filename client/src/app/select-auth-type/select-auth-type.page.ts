e0130a8aedbbed2ffd7ab3da39ef8d131d6cbcb8
Isaac Wahout <isaacwahout@Isaacs-MacBook-Pro.local>
2021-12-05 22:16:47 -0800

    adding one more page

---
+++ client/src/app/select-auth-type/select-auth-type.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-auth-type',
+  templateUrl: './select-auth-type.page.html',
+  styleUrls: ['./select-auth-type.page.scss'],
+})
+export class SelectAuthTypePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
